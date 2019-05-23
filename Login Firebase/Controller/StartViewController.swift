84667f91784638845db050f6c991bd900252abdc
Saiful <icungse@gmail.com>
2019-05-24 02:26:37 +0700

    session for logged in user

--- Login Firebase/Controller/StartViewController.swift
+++ Login Firebase/Controller/StartViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class StartViewController: UIViewController {
 
@@ -17,14 +18,10 @@ class StartViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if Auth.auth().currentUser != nil {
+            self.performSegue(withIdentifier: "alreadyLogin", sender: nil)
+        }
     }
-    */
-
 }
