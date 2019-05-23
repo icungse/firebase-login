6610cbb3c68807bb1d7bc9bced101c49a9b5e9b8
Saiful <icungse@gmail.com>
2019-05-24 01:06:48 +0700

    Initial Commit

---
+++ Login Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login Firebase
+//
+//  Created by icungse on 24/05/19.
+//  Copyright © 2019 icung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
